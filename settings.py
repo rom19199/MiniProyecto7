49a2ae870eb2d21dad5ca306f758d5ebe1eb8661
rom19199 <54779748+rom19199@users.noreply.github.com>
2022-10-25 08:41:12 -0600

    Documentación final

--- settings.py
+++ settings.py
@@ -1,19 +1,20 @@
 # Ajustes globales
 
 # Pantalla
-SCALE = 5
-SCREEN = 100
+SCALE = 8
+SCREEN = 75
 WIDTH = SCREEN * SCALE
 HEIGHT = SCREEN * SCALE
 
 # Datos de juego
 JUGADOR = 0
 PELOTA = 1
-SCORE = 9999
-EMPTY = 999
+SCORE = 199
+EMPTY = 8888
 
 # Colores
 WHITE = (255, 255, 255)
-GREEN = (35, 192, 56)
+#GREEN = (35, 192, 56)
+GREEN = (34,139,34)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
